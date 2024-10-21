6983e0e57d013e64827ab265f411deefdf211afb
qiura122 <qiura122@users.noreply.github.com>
2024-10-21 11:16:53 +0800

    t

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>Tentang Emo</h1>
-<p>Halo Nama saya <?= $data['nama']; ?>, saya seorang <?= $data['pekerjaan']; ?></p>
+<div class="container">
+    <h1 class="mt-3">Tentang Emo</h1>
+    <img src="<?php BASEURL; ?>img/Profile.JPG" alt="Komang Emo" width="200" class="rounded-circle shadow">
+    <p>Halo Nama saya <?= $data['nama']; ?>, saya seorang <?= $data['pekerjaan']; ?></p>
+</div>
--- app/views/tamplates/header.php
+++ app/views/tamplates/header.php
@@ -5,6 +5,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman <?= $data['judul']; ?></title>
+    <link href="<?php BASEURL; ?>css/bootstrap.css" rel="stylesheet">
 </head>
 
 <body>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="<?php BASEURL; ?>Home">PHP MVC</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="<?php BASEURL; ?>Home">Home</a>
+                    <a class="nav-link" href="<?php BASEURL; ?>About">About</a>
+                </div>
+            </div>
+        </div>
+    </nav>
